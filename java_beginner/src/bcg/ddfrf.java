1ae0eae9ed30b05138674cf0aaadb2ff95f80b76
AromA <AromADSH@gmail.com>
2022-04-11 13:00:38 +0900

    ing

---
+++ java_beginner/src/bcg/ddfrf.java
@@ -0,0 +1,16 @@
+package bcg;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class ddfrf {
+	static int Q1, Q2, Q3, Q4, Q5, Q6, k, sum;
+	   static int count = 0;
+	   static String C, A, B;
+
+	   public static void main(String[] args) {
+	      Scanner scan = new Scanner(System.in);
+	      reader("./Bcg.txt");
+
+}
+}
